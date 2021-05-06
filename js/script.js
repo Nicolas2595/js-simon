b438417b29ce513a3dd483aa4d2cebc4851fbc23
Nicolas2595 <nmorelli10@gmail.com>
2021-05-06 16:03:31 +0200

    simon says

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/*
+Un alert() espone 5 numeri generati casualmente.
+Da li parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+// FUNZIONE
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+// FINE FUNZIONE
+
+// VARIABILI
+var listaNumeriCasuali = [];
+var listaNumeriPrompt = [];
+var numeroCasuale;
+var numeroPrompt;
+
+// Un alert espone i 5 numeri generati casualmente
+for (var i = 0; i < 5; i++) {
+numeroCasuale = getRandomNumber(1, 100);
+console.log(numeroCasuale);
+listaNumeriCasuali.push(numeroCasuale);
+console.log(listaNumeriCasuali);
+}
+alert("Memorizza questi 5 numeri: " + listaNumeriCasuali);
+
+// Timer di 30 secondi più inserimento dei numeri
+setTimeout(function() {
+    for (var i = 0; i < 5; i++) {
+        numeroPrompt = parseInt(prompt("Inserisci i numeri che hai visto in precedenza"));
+        console.log(numeroPrompt);
+
+        for (var a = 0; a < listaNumeriCasuali.length; a++) {
+            if (numeroPrompt == listaNumeriCasuali[a]) {
+                listaNumeriPrompt.push(numeroPrompt);
+            }
+        }
+    }
+    alert("Hai indovinato " + listaNumeriPrompt.length + " numero/i: il/i: " + listaNumeriPrompt)
+    console.log(listaNumeriPrompt);
+
+}, 3000);
